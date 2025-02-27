cfa6b2e04e8729d3fc91186018fe7d53e959dfc3
Andreo Romera <andreoromera@gmail.com>
2025-02-27 13:04:54 -0300

    Comentários

--- backend/Ambev.Dev.Test.Data/Repositories/UserRepository.cs
+++ backend/Ambev.Dev.Test.Data/Repositories/UserRepository.cs
@@ -2,12 +2,17 @@
 using Ambev.Dev.Test.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ambev.Dev.Test.Data.Repositories
+namespace Ambev.Dev.Test.Data.Repositories;
+
+/// <summary>
+/// User Repository
+/// </summary>
+public class UserRepository(DefaultContext context) : IUserRepository
 {
-    public class UserRepository(DefaultContext context) : IUserRepository
-    {
-        public async Task<User> GetByEmail(string email, CancellationToken cancellationToken) => await context
-            .Users
-            .FirstOrDefaultAsync(x => x.Email.ToLower() == email.ToLower(), cancellationToken);
-    }
+    /// <summary>
+    /// Get the user by email
+    /// </summary>
+    public async Task<User> GetByEmail(string email, CancellationToken cancellationToken) => await context
+        .Users
+        .FirstOrDefaultAsync(x => x.Email.ToLower() == email.ToLower(), cancellationToken);
 }
